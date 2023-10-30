a2bdab1a9f294ba22d178dec74fd0533376a3d07
Saqlain788 <saqlain788@gmail.com>
2023-10-30 13:11:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let loop = true;
+
+while (loop) {
+  const answers: {
+    todo: string;
+    addMore: boolean;
+  } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "todo",
+      message: "What do you want in your todo list?",
+    },
+    {
+      type: "confirm",
+      name: "addMore",
+      message: "Do you want to add more todo?",
+      default: false,
+    },
+  ]);
+  const { todo, addMore } = answers;
+  console.log(answers);
+  loop = addMore;
+  if (todo) {
+    todos.push(todo);
+  } else {
+    console.log("Kindly add valid input");
+  }
+}
+
+if (todos.length > 0) {
+  console.log("Your todo list:");
+  todos.forEach((todo) => {
+    console.log(todo);
+  });
+} else {
+  console.log("No todos found in your list ");
+}
